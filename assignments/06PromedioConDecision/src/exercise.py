db3903a4a9a4a7522a88e0957f6680952991e661
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-09-06 10:02:55 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,9 +1,26 @@
 def main():
     #escribe tu código abajo de esta línea+
-    import statistics
-    list = range(0,100)
-    mean = statistics.mean(list)
-    print(mean)
+    count=0
+    add=0
+    n=1
+
+    while True:
+        n =float(input('Inserte un número (inserta uno negativo para acabar el programa):'))
+   
+        if n < 0:
+            break
+
+         add+=n
+         count+=1
+
+         if count == 0:
+            print('No digitó ningun número')
+    
+         else:
+            promedio= add/count
+
+     print("El promedio es : ",promedio)
+
 
     pass
 if __name__=='__main__':
