367d1e0565916351e5f2eff7a4a1766294461102
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-09-06 10:26:50 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,6 +1,20 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    x = 0
+    y = 1
+    z = 0
+    while True:
+        l =int(input("Inserta un numero mayor a cero"))
+        if l >= 0 :
+        break
+    print("0", end ="")
+    for k in range (1, l):
+        z = x+y
+        print(f"{z}", end = "")
+        x = y
+        y = z
+    print("")   
     pass
 
 if __name__=='__main__':
