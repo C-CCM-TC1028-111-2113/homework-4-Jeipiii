20e0246ed57fd4629bbb3cdd0ce2063b07d307f7
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-09-06 09:01:46 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
-    #escribe tu código abajo de esta línea
+    #escribe tu código abajo de esta línea+
+    import statistics
+    list = range(0,100)
+    mean = statistics.mean(list)
+    print(mean)
+
     pass
 if __name__=='__main__':
     main()
