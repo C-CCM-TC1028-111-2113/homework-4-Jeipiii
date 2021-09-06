423cf61987492ae562916891bf3b928fce90e38f
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-09-06 10:12:03 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    l=int(input("Pon la cantidad de numeros a repetir"))
+    for k in range(l):
+        if k%2 == 0:
+            print('#')
+
+        else:
+            print('%')
     pass
 
 if __name__=='__main__':   
